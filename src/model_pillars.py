f8d7ccbe725d8af96b2d70f91b350cdfd060958c
JordanLevy99 <jdlevy@ucsd.edu>
2021-04-11 11:53:45 -0700

    added some data, updated src, got predictions, and shap figures

--- src/model_pillars.py
+++ src/model_pillars.py
@@ -13,14 +13,18 @@ from src.etl import *
 import random
 
 class PillarExplainer:
-    def __init__(self, mod='lasso', random_state=42, test_size=0.25, alpha=0.5):
+    def __init__(self, mod='lasso', random_state=42, test_size=0.25, alpha=0.5, include_svr=True):
         self.pillar_train, self.pillar_test = read_in_pillars(remove_star=True)
         self.random_state = random_state
         self.test_size = test_size
         self.alpha = alpha
         self.mod_str = mod
         self.models = {}
+        self.include_svr = include_svr
         self.pillars = list(self.pillar_train.keys())
+        self.pillar_dict = {'econ':'Economic Quality', 'busi':'Business Environment', 'gove':'Governance',\
+                   'educ':'Education', 'heal':'Health', 'safe':'Safety and Security', 'pers':'Personal Freedom',\
+                   'soci':'Social Capital', 'envi':'Natural Environment'}
         warnings.filterwarnings("ignore")
 
     def standardize(self, X):
@@ -45,7 +49,9 @@ class PillarExplainer:
 
     def ensemble(self, pillar, X_train, y_train, X_val, y_val):
         model_dict = {}
-        for mod in ['svr', 'lasso', 'elastic']:
+        model_lst = ['lasso', 'elastic']
+        if self.include_svr: model_lst.append('svr')
+        for mod in model_lst:
             score, model = eval('self.'+mod)(pillar, X_train, y_train, X_val, y_val)
             score += random.uniform(-0.00000001, 0.00000001)
             model_dict[mod] = (score, model)
@@ -71,7 +77,7 @@ class PillarExplainer:
         # plot predicted values vs true
         #print(Lreg.predict(X_test))
         score = clf.best_score_
-        print(f"Score of SVR {pillar} model: {score}")
+        print(f"Score of SVR {pillar} model: {score}\n")
         return score, clf.best_estimator_
     
     def lasso(self, pillar, X_train, y_train, X_val, y_val):
@@ -102,7 +108,8 @@ class PillarExplainer:
         clf.fit(X_train, y_train)
         self.models[pillar] = clf.best_estimator_
         score = clf.best_score_
-        print(f'Score of ElasticNet {pillar} model: {score}\n')
+        print(f'Score of ElasticNet {pillar} model: {score}')
+        if self.include_svr: print()
 
         # consider adding y_pred here
 
@@ -118,13 +125,14 @@ class PillarExplainer:
         Used in get_impt_cat()
         """
         # ***use X_test or X_train?
-        explainer = shap.LinearExplainer(model, X_train, feature_dependence="independent")
+        try: explainer = shap.LinearExplainer(model, X_train, feature_dependence="independent")
+        except: explainer = shap.KernelExplainer(model, X_train) #, feature_dependence="independent")
         shap_values = explainer.shap_values(X_val)
         return shap_values
 
 
 
-    def shap_viz_1(self, df_shap,df):
+    def shap_viz_1(self, df_shap, df, feature_list, pillar='total'):
         """
         Returns bar plot of each categories impact on pillar score
         ----
@@ -133,15 +141,14 @@ class PillarExplainer:
         """
         # Make a copy of the input data
         shap_v = pd.DataFrame(df_shap)
-        feature_list = df.columns
+        #feature_list = df.columns
         shap_v.columns = feature_list
-        df_v = df.copy().reset_index().drop('index',axis=1)
-
+        df_v = df.copy()#.reset_index(drop=True)
         # Determine the correlation in order to plot with different colors
         corr_list = list()
-        for i in feature_list:
-            #print(shap_v[i],df_v[i])
-            b = np.corrcoef(shap_v[i],df_v[i])[1][0]
+        for i, feat in enumerate(feature_list):#feature_list:
+#             print(shap_v[feat],df_v[:, i])
+            b = np.corrcoef(shap_v[feat],df_v[:, i])[1][0]
             corr_list.append(b)
         corr_df = pd.concat([pd.Series(feature_list),pd.Series(corr_list)],axis=1).fillna(0)
         # Make a data frame. Column 1 is the feature, and Column 2 is the correlation coefficient
@@ -151,12 +158,16 @@ class PillarExplainer:
         # Plot it
         shap_abs = np.abs(shap_v)
         k=pd.DataFrame(shap_abs.mean()).reset_index()
+#         print(k)
         k.columns = ['Variable','SHAP_abs']
-        k2 = k.merge(corr_df,left_on = 'Variable',right_on='Variable',how='inner')
+        k2 = k.merge(corr_df,on = 'Variable', how='inner')
         k2 = k2.sort_values(by='SHAP_abs',ascending = True)
         colorlist = k2['Sign']
         ax = k2.plot.barh(x='Variable',y='SHAP_abs',color = colorlist, figsize=(5,6),legend=False)
         ax.set_xlabel("SHAP Value (Green = Positive Impact, Red = Negative Impact)")
+        if os.path.exists('figures/shap_plots')==False: os.mkdir('figures/shap_plots')
+        plt.title(f'Feature Importance for {self.pillar_dict[pillar]}');
+        plt.savefig(f'figures/shap_plots/shap_{pillar}.png', dpi=800, bbox_inches='tight');
 
 
     def shap_viz_2(self, shap_values, X_val):
@@ -173,17 +184,22 @@ class PillarExplainer:
     def get_model(self, pillar):
         # split into training and testing dataset
         X_train, X_val, y_train, y_val = self.get_train_val(pillar)
-        X_train, X_val = self.standardize(X_train), self.standardize(X_val)
+        X_train_std, X_val_std = self.standardize(X_train), self.standardize(X_val)
         # model and score of model
-        score, model = eval('self.'+self.mod_str)(pillar, X_train, y_train, X_val, y_val)
+        score, model = eval('self.'+self.mod_str)(pillar, X_train_std, y_train, X_val_std, y_val)
         #^^utilize score???
-        return model, X_train, X_val
+        return model, X_train_std, X_val_std, list(X_train.columns)
 
     def get_models(self):
         for pillar in self.pillars:
             self.get_model(pillar)
         print('Done, check self.models')
 
+    def get_model_explainers(self):
+        for pillar in self.pillars:
+            self.get_impt_cat(pillar)
+        print('Plots saved in figures/shap_plots/')
+        
     def get_impt_cat(self, pillar):
         """
         Outputs viz of impact of categories.
@@ -193,13 +209,13 @@ class PillarExplainer:
         Example run: self.get_impt_cat("busi")
         """
 
-        model, X_train, X_val = self.get_model(pillar)
+        model, X_train, X_val, feature_list = self.get_model(pillar)
 
         # ***use X_test or X_train?
         shap_values = self.make_shap(model, X_train, X_val)
 
         # viz
         # easier to understand version of shap_viz_2
-        self.shap_viz_1(shap_values, X_val)
+        self.shap_viz_1(shap_values, X_val, feature_list, pillar)
         # cooler looking version of shap_viz_1
         #shap_viz_2(shap_values, X_test)
--- src/predict_prosperity.py
+++ src/predict_prosperity.py
@@ -2,6 +2,7 @@ from src.model_pillars import *
 class PredictionModel(PillarExplainer):
     def __init__(self, mod='lasso', random_state=42, test_size=0.25, alpha=0.5):
         super().__init__(mod, random_state, test_size, alpha)
+        self.mod = mod
         self.get_models()
 
         # self.pillar_train, self.pillar_test = read_in_pillars(remove_star=True)
@@ -28,8 +29,9 @@ class PredictionModel(PillarExplainer):
 
         # predict pillar score for each country of each year
         for pillar in self.pillars:
+            std = StandardScaler()
             X_test = self.get_test_data(pillar)
-            res["{0}_preds".format(pillar)] = self.models[pillar].predict(X_test.iloc[:, 4:])
+            res["{0}_preds".format(pillar)] = self.models[pillar].predict(std.fit_transform(X_test.iloc[:, 4:]))
 
         # calcualte prosperity score based on pillar scores
         res["prosperity_preds"] = res.mean(axis=1)
@@ -42,18 +44,28 @@ class PredictionModel(PillarExplainer):
 
         return res
 
-    def determine_ranks(self):
+    def determine_ranks(self, to_csv=False):
 
         df = self.predict_prosperity()
-
+#         df_ranks_15 = pd.DataFrame()
         df_15 = df[df.year == 2015]
-        df_15["prosperity_rank"] = df_15["prosperity_preds"].rank(ascending=False)
+        for pillar in self.pillars:
+            df_15[f'{pillar}_rank'] = df_15[f'{pillar}_preds'].rank(ascending=False).astype(int)
+        df_15["prosperity_rank"] = df_15["prosperity_preds"].rank(ascending=False).astype(int)
         df_15 = df_15.sort_values(by="prosperity_rank", ascending=True)
-
+        
+        
         df_16 = df[df.year == 2016]
-        df_16["prosperity_rank"] = df_16["prosperity_preds"].rank(ascending=False)
+#         df_16 = pd.DataFrame()    
+        for pillar in self.pillars:
+            df_16[f'{pillar}_rank'] = df_16[f'{pillar}_preds'].rank(ascending=False).astype(int)
+        df_16["prosperity_rank"] = df_15["prosperity_preds"].rank(ascending=False)
         df_16 = df_16.sort_values(by="prosperity_rank", ascending=True)
 
         df = df_15.append(df_16)
-
+        if os.path.exists('Processed_Data/') == False: os.mkdir('Processed_Data')
+        if to_csv: 
+            fpath = f'Processed_Data/predictions_{self.mod}.csv'
+            df.to_csv(fpath, index=False)
+            print(f'Saved at {fpath}')
         return df
